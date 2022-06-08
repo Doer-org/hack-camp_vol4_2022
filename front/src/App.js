195027efb93d7fab76a1880be20539fac3157256
nisi0929 <kengo279@icloud.com>
2022-06-08 21:12:48 +0900

    test

--- front/src/App.js
+++ front/src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="font-extrabold">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="font-extrabold">Doer</p>
         <a
           className="App-link"
           href="https://reactjs.org"
