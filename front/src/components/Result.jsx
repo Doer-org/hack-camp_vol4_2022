a29eee7078cf0da71f0c852fadeccf865bfca24b
hikari-8 <kanbarusuruga0127@gmail.com>
2022-06-12 03:06:17 +0900

    suimasenn

--- front/src/components/Result.jsx
+++ front/src/components/Result.jsx
@@ -1,3 +1,51 @@
 export const Result = () => {
-  return <div>Result</div>;
+  return (
+  <section class="text-black">
+    <div class="container px-5 py-24 mx-auto">
+      <div class="flex flex-warp -m-4">
+
+        {/* first column */}
+
+        <div class="p-4 md:w--1/3">
+          <div class="h-full border-2 border-gray-200 border-opacity-50 rounded-md overflow-scroll">
+            <img class="lg:h-52 md:h-40 w-full object-cover object-center" src="https://source.unsplash.com/random"/>
+              <div class="p-6">
+                <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Name</h1>
+                  <h2 class="tracking-widest text-xs title-font font-medium text-gray-600 mb-1">Cost</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime iure repellendus esse alias molestias temporibus, nemo nulla doloremque architecto, blanditiis harum quia? Magnam voluptatem amet in earum expedita porro iure eos et sunt distinctio omnis laudantium quae facilis numquam, asperiores repellendus iusto? Debitis expedita fuga quibusdam reiciendis, labore vero non?</p>          
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* second column */}
+        <div class="p-4 md:w--1/3">
+          <div class="h-full border-2 border-gray-200 border-opacity-50 rounded-md overflow-scroll">
+            <img class="lg:h-52 md:h-40 w-full object-cover object-center" src="https://source.unsplash.com/random"/>
+            <div class="p-6">
+              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Name</h1>
+                <h2 class="tracking-widest text-xs title-font font-medium text-gray-600 mb-1">Cost</h2>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime iure repellendus esse alias molestias temporibus, nemo nulla doloremque architecto, blanditiis harum quia? Magnam voluptatem amet in earum expedita porro iure eos et sunt distinctio omnis laudantium quae facilis numquam, asperiores repellendus iusto? Debitis expedita fuga quibusdam reiciendis, labore vero non?</p>          
+            </div>
+          </div>
+          </div>
+
+        {/* third column*/}
+
+        <div class="p-4 md:w--1/3">
+          <div class="h-full border-2 border-gray-200 border-opacity-50 rounded-md overflow-scroll">
+            <img class="lg:h-52 md:h-40 w-full object-cover object-center" src="https://source.unsplash.com/random"/>
+            <div class="p-6">
+              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Name</h1>
+                <h2 class="tracking-widest text-xs title-font font-medium text-gray-600 mb-1">Cost</h2>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime iure repellendus esse alias molestias temporibus, nemo nulla doloremque architecto, blanditiis harum quia? Magnam voluptatem amet in earum expedita porro iure eos et sunt distinctio omnis laudantium quae facilis numquam, asperiores repellendus iusto? Debitis expedita fuga quibusdam reiciendis, labore vero non?</p>          
+            </div>
+          </div>
+        </div>
+      </div>
+
+  </section>
+);
 };
+
+
