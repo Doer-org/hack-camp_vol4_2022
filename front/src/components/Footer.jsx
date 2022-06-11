c53eb6bed327cfb4e5a57c2c450ce061de4789a1
Mao Yamaguchi <mao@MacBook.local>
2022-06-12 01:44:41 +0900

    dev-front fix header & footer

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -2,8 +2,8 @@ import "../App.css";
 export const Footer = () => {
 
   return (
-  <div class="p-10 main-color justify-center flex">
-  <p class="text-black text-xl lg:text-3xl font-bold">
+  <div className="p-10 main-color justify-center flex">
+  <p className="text-white text-xl lg:text-3xl font-bold">
     OKASHI OMIKUZI
   </p>
 </div>
