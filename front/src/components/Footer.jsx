bbf48b5e07e8e3655b337cf78b2f80839c13456d
hikari-8 <kanbarusuruga0127@gmail.com>
2022-06-12 00:42:59 +0900

    adding text responsive design

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -3,7 +3,7 @@ export const Footer = () => {
 
   return (
   <div class="p-10 main-color justify-center flex">
-  <p class="text-black text-3xl font-bold">
+  <p class="text-black text-xl lg:text-3xl font-bold">
     OKASHI OMIKUZI
   </p>
 </div>
