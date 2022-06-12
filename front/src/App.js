cceda620187ea40968720e85cabdead38749c97b
hikari-8 <kanbarusuruga0127@gmail.com>
2022-06-13 01:19:25 +0900

    changing classname and padding

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import { Home } from "./components/Home";
 import { Header } from "./components/Header";
-import { Result } from "./components/Result";
 import { Footer } from "./components/Footer";
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <div className="App">
       <Header />
       <Home />
-      <Result />
       <Footer />
     </div>
   );
