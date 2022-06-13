941074cffcbecb6fa482670b80bad8a5ece88fca
nisi0929 <kengo279@icloud.com>
2022-06-14 00:48:35 +0900

    add About.css

--- front/src/components/About.jsx
+++ front/src/components/About.jsx
@@ -1,15 +1,15 @@
 import users from "../data/users.json";
-import "../App.css";
+import "../About.css";
 export const About = () => {
   return (
     <div className="text-center">
       <p className="text-xl lg:text-3xl font-bold md:my-20 my-10">About</p>
-      <div className="grid grid-cols-1 md:grid-cols-2 mx-5 md:mx-20 lg:mx-40">
+      <div className="grid grid-cols-1 md:grid-cols-2 mx-5 md:mx-20 lg:mx-40 ">
         {users.map((user, i) => {
           return (
             <div
               key={i}
-              className="grid grid-cols-1 lg:grid-cols-2 items-center justify-items-center rounded-2xl shadow-2xl m-5 md:m-10 md:p-10 p-5 break-all"
+              className="grid grid-cols-1 lg:grid-cols-2 items-center justify-items-center rounded-2xl shadow-2xl m-5 md:m-10 md:p-10 p-5 break-all oneArea"
             >
               <img
                 src={user.image}
