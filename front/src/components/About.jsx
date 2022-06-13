381064e6a84037486594a49b43bfd42f4bfedd3a
nisi0929 <kengo279@icloud.com>
2022-06-14 00:40:47 +0900

    fix padding && margin

--- front/src/components/About.jsx
+++ front/src/components/About.jsx
@@ -3,13 +3,13 @@ import "../App.css";
 export const About = () => {
   return (
     <div className="text-center">
-      <p className="text-xl lg:text-3xl font-bold my-20">About</p>
-      <div className="grid grid-cols-1 lg:grid-cols-2 md:mx-60">
+      <p className="text-xl lg:text-3xl font-bold md:my-20 my-10">About</p>
+      <div className="grid grid-cols-1 md:grid-cols-2 mx-5 md:mx-20 lg:mx-40">
         {users.map((user, i) => {
           return (
             <div
               key={i}
-              className="grid grid-cols-1 lg:grid-cols-2 items-center justify-items-center rounded-2xl shadow-lg lg:m-10 md:p-10 p-5 break-all"
+              className="grid grid-cols-1 lg:grid-cols-2 items-center justify-items-center rounded-2xl shadow-2xl m-5 md:m-10 md:p-10 p-5 break-all"
             >
               <img
                 src={user.image}
