54e3008e2341c41b573df57f6a403e46c23473f6
mahiro72 <mahiro.yoshie@gmail.com>
2022-06-18 04:13:45 +0900

    refactor

--- front/src/components/layouts/Footer.jsx
+++ front/src/components/layouts/Footer.jsx
@@ -1,4 +1,4 @@
-import "../App.css";
+import "../../App.css";
 export const Footer = () => {
 
   return (
--- front/src/components/layouts/Header.jsx
+++ front/src/components/layouts/Header.jsx
@@ -1,5 +1,5 @@
 import { HeadHighlight } from "./HeadHighlight";
-import "../App.css";
+import "../../App.css";
 import {Navbar} from "./Navbar"
 
 export const Header = () => {
