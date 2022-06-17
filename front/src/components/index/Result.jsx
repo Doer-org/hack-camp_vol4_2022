97bb1b57ea01ccfbb5c85d587bf5307a12cee598
mahiro72 <mahiro.yoshie@gmail.com>
2022-06-18 06:29:20 +0900

    refctor

--- front/src/components/index/Result.jsx
+++ front/src/components/index/Result.jsx
@@ -1,4 +1,4 @@
-import "../../App.css";
+import "../../styles/App.css";
 export const Result = () => {
   return (
   //omikuzi
--- front/src/components/layouts/Header.jsx
+++ front/src/components/layouts/Header.jsx
@@ -1,4 +1,4 @@
-import "../../App.css";
+import "../../styles/App.css";
 import {Navbar} from "./Navbar"
 
 export const Header = () => {
