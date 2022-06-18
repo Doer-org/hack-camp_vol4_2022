035cbe793c09b64357e7a24e5f9f39c9a92f33f9
mahiro72 <mahiro.yoshie@gmail.com>
2022-06-18 15:30:38 +0900

    add: ramen

---
+++ front/src/components/layouts/night/NightFooter.jsx
@@ -0,0 +1,12 @@
+import "../../../styles/App.css";
+export const NightFooter = () => {
+
+  return (
+    <div className="p-10 night-color justify-center flex">
+        <p className="text-white text-xl lg:text-3xl font-bold">
+            RAMEN OMIKUZI
+        </p>
+    </div>
+);
+};
+
