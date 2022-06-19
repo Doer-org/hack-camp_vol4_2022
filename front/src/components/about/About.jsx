67d40c0ac08e479184b8a699b163f5a61041912c
nisi0929 <kengo279@icloud.com>
2022-06-19 13:16:24 +0900

    fix-about-page

--- front/src/components/about/About.jsx
+++ front/src/components/about/About.jsx
@@ -1,9 +1,39 @@
 import users from "../../data/users.json";
-import "../../styles/about/About.css"
+import "../../styles/about/About.css";
 export const About = () => {
   return (
     <div className="text-center">
       <p className="text-xl lg:text-3xl font-bold md:my-20 my-10">About</p>
+
+      <div className="mt-2 sm:mt-30 md:mt-25 lg:mt-50 ">
+        <div className="shadow-2xl rounded-br-3xl rounded-3xl mx-2 my-2 lg:mx-28 ">
+          <div className="main-color p-2 m-25 rounded-tl-3xl rounded-tr-3xl font-bold text-white md:text-xl ">
+            {" "}
+            作った経緯{" "}
+          </div>
+          <div className="p-4 ">
+            お買い物でお菓子を買うとき、ただ選ぶのではなく、ちょっとひと工夫あれば面白いかなと思って作りました。
+          </div>
+          <div className="p-6 ">
+            自分の感情からお菓子を提案したり、値段を決めてその金額内でいくつかお菓子を提案します。
+          </div>
+        </div>
+
+        <div className="shadow-2xl rounded-3xl mt-10 mb-10 mx-2 my-2 lg:mx-28 ">
+          <div className="main-color p-2 m-25 font-bold rounded-tl-3xl rounded-tr-3xl text-white md:text-xl ">
+            Skills
+          </div>
+          <div className="p-4 ">
+            Frontend：JavaScript, React, tailwind.css
+            <br />
+            Backend: Golang, Gin <br />
+            その他：PostgreSQL, Docker, Heroku, Vercel
+          </div>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 mx-5 md:mx-32 lg:mx-32 xl:mx-80"></div>
+
       <div className="grid grid-cols-1 lg:grid-cols-2 mx-5 md:mx-32 lg:mx-32 xl:mx-80 ">
         {users.map((user, i) => {
           return (
@@ -19,7 +49,7 @@ export const About = () => {
               <div>
                 <a
                   href={`https://github.com/${user.name}`}
-                  className="text-3xl font-medium"
+                  className="text-2xl font-medium lg:text-white "
                 >
                   {user.name}
                 </a>
